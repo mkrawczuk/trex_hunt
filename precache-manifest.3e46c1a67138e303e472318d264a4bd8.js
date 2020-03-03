009e77fa3a74ce92df34281f88a61411ff0e86da
Mateusz Krawczuk <krawczukmat@gmail.com>
2020-03-03 17:52:42 +0100

    Updates

---
+++ precache-manifest.3e46c1a67138e303e472318d264a4bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0688a7f388bc255f122b8d38fc42c4d7",
+    "url": "/trex_hunt/index.html"
+  },
+  {
+    "revision": "848cb23ad24d090bcf4c",
+    "url": "/trex_hunt/static/css/main.e710e9df.chunk.css"
+  },
+  {
+    "revision": "1bdc4fe0403e8746a4ec",
+    "url": "/trex_hunt/static/js/2.d66be5ab.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/trex_hunt/static/js/2.d66be5ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848cb23ad24d090bcf4c",
+    "url": "/trex_hunt/static/js/main.606aafff.chunk.js"
+  },
+  {
+    "revision": "23c66690bde9ef1a3212",
+    "url": "/trex_hunt/static/js/runtime-main.317e636e.js"
+  }
+]);
